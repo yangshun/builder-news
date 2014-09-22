ea30e55ca489812b81b7197939598e11ddaa9343
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-09-22 17:54:09 +0800

    Reordered the pages

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 $(function () {
-  $('.segmented').UIPanelToggle('#toggle-panels',function(){$.noop;});
-  $('#bookmarks-nav').on('singletap', function() {
+  $('.segmented').UIPanelToggle('#toggle-panels',function (){
+    $.noop;
+  });
+  
+  $('#bookmarks-nav').on('singletap', function () {
     $.UIGoToArticle('#bookmarks');
   });
 
   $.UITabbar({
-    tabs : 3,
-    icons : ['fa fa-info-circle fa-2x', 'fa fa-newspaper-o fa-2x', 'fa fa-star-o fa-2x'],
-    labels : ['About', 'Home', 'Favorites'],
-    selected : 2
+    tabs: 3,
+    icons: ['fa fa-newspaper-o fa-2x', 'fa fa-star-o fa-2x', 'fa fa-info-circle fa-2x'],
+    labels: ['Home', 'Favorites', 'About'],
+    selected: 1
   });
 
   $('.bn-about-list li').on('singletap', function () {
